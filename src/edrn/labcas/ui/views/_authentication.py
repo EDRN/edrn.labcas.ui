e6ba192bdf25f990347adeee73024345720a0e5a
Sean Kelly <kelly@seankelly.biz>
2016-01-12 18:06:40 -0600

    redo login form using deform

--- src/edrn/labcas/ui/views/_authentication.py
+++ src/edrn/labcas/ui/views/_authentication.py
@@ -5,11 +5,37 @@ from edrn.labcas.ui import PACKAGE_NAME
 from pyramid.security import remember, forget
 from pyramid.httpexceptions import HTTPFound
 from pyramid_ldap import get_ldap_connector
+from deform import widget
+import colander, deform
+
+
+class _LoginSchema(colander.MappingSchema):
+    u'''Schema for a login page'''
+    username = colander.SchemaNode(
+        colander.String(),
+        title=u'EDRN Username',
+        description=u'Your account name within the Early Detection Research Network.',
+        validator=colander.Length(min=1)
+    )
+    password = colander.SchemaNode(
+        colander.String(),
+        title=u'Password',
+        description=u'The secret sequence of characters that authenticates your username.',
+        validator=colander.Length(min=1),
+        widget=widget.PasswordWidget()
+    )
 
 
 class AuthenticationView(object):
     def __init__(self, request):
         self.request = request
+    @property
+    def form(self):
+        schema = _LoginSchema()
+        return deform.Form(schema, buttons=('submit',))
+    @property
+    def widgetResources(self):
+        return self.form.get_widget_resources()
     @view_config(route_name='login', renderer=PACKAGE_NAME + ':templates/login.pt')
     @forbidden_view_config(renderer=PACKAGE_NAME + ':templates/login.pt')
     def login(self):
@@ -19,7 +45,7 @@ class AuthenticationView(object):
             referrer = '/'
         cameFrom = self.request.params.get('cameFrom', referrer)
         message = username = password = u''
-        if 'form.submitted' in self.request.params:
+        if 'submit' in self.request.params:
             username, password = self.request.params['username'], self.request.params['password']
             connector = get_ldap_connector(self.request)
             result = connector.authenticate(username, password)
@@ -33,7 +59,8 @@ class AuthenticationView(object):
             url=self.request.application_url + '/login',
             cameFrom=cameFrom,
             username=username,
-            message=message
+            message=message,
+            form=self.form.render()
         )
     @view_config(route_name='logout')
     def logout(self):
