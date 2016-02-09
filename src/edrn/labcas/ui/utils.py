5eaf615a0893369f7b2b1752725935961b5ac3f2
Sean Kelly <kelly@seankelly.biz>
2016-02-09 17:05:26 -0600

    add class for representing labcas workflows

--- src/edrn/labcas/ui/utils.py
+++ src/edrn/labcas/ui/utils.py
@@ -3,7 +3,16 @@
 
 
 class LabCASProduct(object):
+    u'''A product stored within LabCAS.'''
     def __init__(self, identifier, name):
         self.identifier, self.name = identifier, name
     def __cmp__(self, other):
         return cmp(self.name, other.name)
+
+
+class LabCASWorkflow(object):
+    u'''A workflow we can execute within LabCAS.'''
+    def __init__(self, identifier, name, conditions, tasks):
+        self.identifier, self.name, self.conditions, self.tasks = identifier, name, conditions, tasks
+    def __cmp__(self, other):
+        return cmp(self.identifier, other.identifier)
