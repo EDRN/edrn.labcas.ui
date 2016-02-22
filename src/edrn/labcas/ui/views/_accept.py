4832a1a37b9163335d5f0182777c2f4ab5419c32
Sean Kelly <kelly@seankelly.biz>
2016-02-22 13:24:22 -0600

    Add existing file list, implement saving of files, clean up buttons, clarify wording

--- src/edrn/labcas/ui/views/_accept.py
+++ src/edrn/labcas/ui/views/_accept.py
@@ -1,10 +1,9 @@
 # encoding: utf-8
 
 from edrn.labcas.ui import PACKAGE_NAME
-from edrn.labcas.ui.interfaces import IBackend
-from edrn.labcas.ui.utils import LabCASWorkflow
 from pyramid.view import view_config, view_defaults
-from zope.component import getUtility
+from pyramid.response import Response
+import os.path, json, httplib, os
 
 
 @view_defaults(renderer=PACKAGE_NAME + ':templates/accept.pt')
@@ -13,4 +12,28 @@ class AcceptView(object):
         self.request = request
     @view_config(route_name='accept', permission='upload')
     def __call__(self):
-        return {'metadataForm': self.request.session['metadataForm']}
+        if u'data' in self.request.params:
+            if 'name' not in self.request.params or 'file' not in self.request.params:
+                return Response(json.dumps({
+                    u'OK': 0,
+                    u'info': u'Missing "name" and/or "file" form elements'
+                }), httplib.OK, content_type='application/json')
+            filename, f = self.request.params['name'], self.request.params['file']
+            target = os.path.join(self.request.session['datasetDir'], filename)
+            with open(target, 'wb') as out:
+                while True:
+                    buf = f.file.read(f.bufsize)
+                    if len(buf) == 0: break
+                    out.write(buf)
+            return Response(json.dumps({u'OK': 1}), httplib.OK, content_type='application/json')
+        entries = os.listdir(self.request.session['datasetDir'])
+        try:
+            del entries[entries.index('DatasetMetadata.xmlmet')]
+        except ValueError:
+            pass
+        entries.sort()
+        return {
+            'hasFiles': len(entries) > 0,
+            'currentFiles': entries,
+            'metadataForm': self.request.session['metadataForm']
+        }
