dbb6ac89f91c47d6da6c6de9614648224a47ae14
Sean Kelly <kelly@seankelly.biz>
2016-02-23 11:09:33 -0600

    poke the backend when done

--- src/edrn/labcas/ui/views/_accept.py
+++ src/edrn/labcas/ui/views/_accept.py
@@ -1,8 +1,11 @@
 # encoding: utf-8
 
 from edrn.labcas.ui import PACKAGE_NAME
-from pyramid.view import view_config, view_defaults
+from edrn.labcas.ui.interfaces import IBackend
+from pyramid.httpexceptions import HTTPFound
 from pyramid.response import Response
+from pyramid.view import view_config, view_defaults
+from zope.component import getUtility
 import os.path, json, httplib, os
 
 
@@ -13,6 +16,7 @@ class AcceptView(object):
     @view_config(route_name='accept', permission='upload')
     def __call__(self):
         if u'data' in self.request.params:
+            # Incoming file
             if 'name' not in self.request.params or 'file' not in self.request.params:
                 return Response(json.dumps({
                     u'OK': 0,
@@ -26,14 +30,28 @@ class AcceptView(object):
                     if len(buf) == 0: break
                     out.write(buf)
             return Response(json.dumps({u'OK': 1}), httplib.OK, content_type='application/json')
-        entries = os.listdir(self.request.session['datasetDir'])
-        try:
-            del entries[entries.index('DatasetMetadata.xmlmet')]
-        except ValueError:
-            pass
-        entries.sort()
-        return {
-            'hasFiles': len(entries) > 0,
-            'currentFiles': entries,
-            'metadataForm': self.request.session['metadataForm']
-        }
+        elif u'finish' in self.request.params:
+            # Completion
+            backend = getUtility(IBackend)
+            metadata = self.request.session['metadata']
+            # Apparently we need two workflows, not just the one, so this idea isn't going to work:
+            # workflow = self.request.session['workflow']
+            # backend.getWorkflowMgr().executeDynamicWorkflow([workflow.identifier], metadata)
+            backend.getWorkflowMgr().executeDynamicWorkflow(
+                ['urn:edrn:LabcasUploadInitTask', 'urn:edrn:LabcasUploadExecuteTask'],
+                metadata
+            )
+            return HTTPFound(self.request.route_url('home'))
+        else:
+            # Presentation
+            entries = os.listdir(self.request.session['datasetDir'])
+            try:
+                del entries[entries.index('DatasetMetadata.xmlmet')]
+            except ValueError:
+                pass
+            entries.sort()
+            return {
+                'hasFiles': len(entries) > 0,
+                'currentFiles': entries,
+                'metadataForm': self.request.session['metadataForm']
+            }
