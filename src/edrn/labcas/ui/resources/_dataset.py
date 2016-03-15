441d99f8ccf08f0d59797e2a72ca68570df10ac6
Sean Kelly <kelly@seankelly.biz>
2016-03-15 10:09:56 -0500

    Allow super users to view any dataset

--- src/edrn/labcas/ui/resources/_dataset.py
+++ src/edrn/labcas/ui/resources/_dataset.py
@@ -3,18 +3,22 @@
 from pyramid.security import Allow, DENY_ALL
 from zope.component import getUtility
 from edrn.labcas.ui.interfaces import IBackend
+from edrn.labcas.ui.utils import SUPER_GROUP
 
 
 class Dataset(object):
     def __init__(self, request):
         principals = frozenset(request.effective_principals)
-        datasetID = request.matchdict.get('datasetID', None)
-        backend = getUtility(IBackend)
-        product = backend.getFileMgr().getProductTypeById(datasetID)
-        typeMetadata = product.get('typeMetadata', {})
-        owners = frozenset(typeMetadata.get('OwnerGroup', []))
-        intersection = principals & owners
-        if len(intersection) > 0:
-            self.__acl__ = [(Allow, principal, 'view') for principal in intersection]
+        if SUPER_GROUP in principals:
+            self.__acl__ = [(Allow, SUPER_GROUP, 'view')]
         else:
-            self.__acl__ = [DENY_ALL]
+            datasetID = request.matchdict.get('datasetID', None)
+            backend = getUtility(IBackend)
+            product = backend.getFileMgr().getProductTypeById(datasetID)
+            typeMetadata = product.get('typeMetadata', {})
+            owners = frozenset(typeMetadata.get('OwnerGroup', []))
+            intersection = principals & owners
+            if len(intersection) > 0:
+                self.__acl__ = [(Allow, principal, 'view') for principal in intersection]
+            else:
+                self.__acl__ = [DENY_ALL]
