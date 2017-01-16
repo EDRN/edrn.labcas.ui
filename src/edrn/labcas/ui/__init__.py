eb01aa0f031e3233a659f9009d7447c3642d1f05
Sean Kelly <kelly@seankelly.biz>
2017-01-16 11:54:41 -0600

    re-fix CA-1474

--- src/edrn/labcas/ui/__init__.py
+++ src/edrn/labcas/ui/__init__.py
@@ -64,6 +64,7 @@ def main(global_config, **settings):
     config.set_authorization_policy(ACLAuthorizationPolicy())
     config.add_static_view('static', 'static', cache_max_age=3600)  # use ini?
     config.add_static_view('deform_static', 'deform:static')
+    config.add_route('home', '/')
     config.add_route('collections', '/c', factory=Collections)
     config.add_route('collection', '/c/{collectionID}', factory=Collection)
     config.add_route('dataset', '/c/{collectionID}/{datasetID}', factory=Dataset)
---
+++ src/edrn/labcas/ui/views/_home.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+from edrn.labcas.ui import PACKAGE_NAME
+from pyramid.view import view_config, view_defaults
+from pyramid.httpexceptions import HTTPFound
+
+
+@view_defaults(renderer=PACKAGE_NAME + ':templates/home.pt')
+class HomeView(object):
+    def __init__(self, request):
+        self.request = request
+    @view_config(route_name='home')
+    def __call__(self):
+        return HTTPFound(self.request.route_url('collections'))
