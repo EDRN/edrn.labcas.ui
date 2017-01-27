77ed73eba7f461f0d03e67cfcd3b9328abee7318
Sean Kelly <kelly@seankelly.biz>
2017-01-27 11:29:20 -0600

    major fist bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 # ------------
 
 _name            = 'edrn.labcas.ui'
-_version         = '0.0.3'
+_version         = '1.0.0'
 _description     = 'User interface for LabCAS, for EDRN'
 _author          = 'Sean Kelly'
 _authorEmail     = 'sean.kelly@jpl.nasa.gov'
