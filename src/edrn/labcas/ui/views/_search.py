9692a096a6d1a3661182193bc19d592d411a7097
Sean Kelly <kelly@seankelly.biz>
2017-07-13 15:29:40 -0500

    Initial stab at CA-1550

--- src/edrn/labcas/ui/views/_search.py
+++ src/edrn/labcas/ui/views/_search.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
-from pyramid.view import view_config, view_defaults
 from edrn.labcas.ui import PACKAGE_NAME
+from edrn.labcas.ui.interfaces import IBackend
+from pyramid.view import view_config, view_defaults
+from zope.component import getUtility
 
 
 @view_defaults(renderer=PACKAGE_NAME + ':templates/search.pt')
@@ -10,4 +12,66 @@ class SearchView(object):
         self.request = request
     @view_config(route_name='search', permission='view')
     def __call__(self):
-        return {}
+        keywords = self.request.params.get('search')
+        backend = getUtility(IBackend)
+        collections = backend.getSearchEngine(u'collections').select(
+            q=keywords,
+            fields=None,
+            highlight=None,
+            score=True,
+            sort=['CollectionName'],
+            start=0,
+            rows=99999  # FIXME: we should support pagination
+        )
+        collections = [{
+            u'name': i[u'CollectionName'],
+            u'url': self.request.route_url('collection', collectionID=i[u'id'])
+            # Other potential metadata to include here:
+            # Discipline, Organ, InstitutionId, ProtocolId, CollaborativeGroup, LeadPI, Consortium, QAState,
+            # Institution, OrganId, LeadPIId, DataCustodian, CollectionDescription, ProtocolName', OwnerPrincipal
+        } for i in collections.results]
+        datasets = backend.getSearchEngine(u'datasets').select(
+            q=keywords,
+            fields=None,
+            highlight=None,
+            score=True,
+            sort=['DatasetName'],
+            start=0,
+            rows=99999  # FIXME: we should support pagination
+        )
+        datasets = [{
+            u'name': i[u'DatasetName'],
+            u'url': self.request.route_url('dataset', collectionID=i[u'CollectionId'], datasetID=i[u'id'])
+            # Other potential metadata to include here:
+            # Cohort, CollectionName, CollectionId, Species, DatasetVersion, DatasetDescription, Sub-Cohort
+        } for i in datasets.results]
+        files = backend.getSearchEngine(u'files').select(
+            q=keywords,
+            fields=None,
+            highlight=None,
+            score=True,
+            sort=['FileName'],
+            start=0,
+            rows=99999  # FIXME: we should support pagination
+        )
+        files = [{
+            u'name': i[u'FileName'],
+            u'url': self.request.route_url(
+                'file', collectionID=i[u'CollectionId'], datasetID=i[u'DatasetId'], fileID=i[u'id']
+            )
+            # Other potential metadata to include here: FileSize
+        } for i in files.results]
+        everything = []
+        for i in collections:
+            everything.append((u'collection', i))
+        for i in datasets:
+            everything.append((u'dataset', i))
+        for i in files:
+            everything.append((u'file', i))
+        everything.sort(lambda a, b: cmp(a[1][u'name'], b[1][u'name']))
+        return {
+            u'collections': collections,
+            u'datasets': datasets,
+            u'files': files,
+            u'everything': everything
+        }
