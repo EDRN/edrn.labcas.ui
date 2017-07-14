bb15e6dbf0f68e881fc97ccdef1442d8c9f0125f
Sean Kelly <kelly@seankelly.biz>
2017-07-14 13:33:46 -0500

    top 10 search

--- src/edrn/labcas/ui/views/_search.py
+++ src/edrn/labcas/ui/views/_search.py
@@ -2,6 +2,7 @@
 
 from edrn.labcas.ui import PACKAGE_NAME
 from edrn.labcas.ui.interfaces import IBackend
+from pyramid.renderers import get_renderer
 from pyramid.view import view_config, view_defaults
 from zope.component import getUtility
 import humanize
@@ -17,6 +18,7 @@ class SearchView(object):
         return unicode(humanize.naturalsize(size)).replace(u' ', u' ')  # There's a NO-BREAK SPACE in there.
     @view_config(route_name='search', permission='view')
     def __call__(self):
+        searchMacros = get_renderer(PACKAGE_NAME + ':templates/searchmacros.pt').implementation()
         keywords = self.request.params.get('search')
         backend = getUtility(IBackend)
         collections = backend.getSearchEngine(u'collections').select(
@@ -37,6 +39,7 @@ class SearchView(object):
             # Discipline, Organ, InstitutionId, ProtocolId, CollaborativeGroup, LeadPI, Consortium, QAState,
             # Institution, OrganId, LeadPIId, DataCustodian, CollectionDescription, ProtocolName', OwnerPrincipal
         } for i in collections.results]
+        numCollections, top10Collections, collections = len(collections), collections[:10], collections[10:]
         datasets = backend.getSearchEngine(u'datasets').select(
             q=keywords,
             fields=None,
@@ -55,6 +58,7 @@ class SearchView(object):
             # Other potential metadata to include here:
             # Cohort, CollectionName, CollectionId, Species, DatasetVersion, DatasetDescription, Sub-Cohort
         } for i in datasets.results]
+        numDatasets, top10Datasets, datasets = len(datasets), datasets[:10], datasets[10:]
         files = backend.getSearchEngine(u'files').select(
             q=keywords,
             fields=None,
@@ -73,8 +77,19 @@ class SearchView(object):
             u'desc': i[u'GrossDescription'] if i.get(u'GrossDescription') else None,
             u'score': self.percent(i[u'score'])
         } for i in files.results]
+        numFiles, top10Files, files = len(files), files[:10], files[10:]
         return {
+            u'searchMacros': searchMacros,
+            u'numCollections': numCollections,
+            u'top10Collections': top10Collections,
             u'collections': collections,
+            u'numRemainingCollections': len(collections),
+            u'numDatasets': numDatasets,
+            u'top10Datasets': top10Datasets,
             u'datasets': datasets,
-            u'files': files
+            u'numRemainingDatasets': len(datasets),
+            u'numFiles': numFiles,
+            u'top10Files': top10Files,
+            u'files': files,
+            u'numRemainingFiles': len(files)
         }
