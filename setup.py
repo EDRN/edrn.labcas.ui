2978fa27a03fe9560ce2eb4d617bfe8616405a3f
Sean Kelly <kelly@seankelly.biz>
2017-08-01 13:47:38 -0500

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 # ------------
 
 _name            = 'edrn.labcas.ui'
-_version         = '1.0.10'
+_version         = '1.0.11'
 _description     = 'User interface for LabCAS, for EDRN'
 _author          = 'Sean Kelly'
 _authorEmail     = 'sean.kelly@jpl.nasa.gov'
